67e90acd7ff95fd083b10d0b0156e7a10256ac28
Ernestoc14 <descreed34@hotmail.com>
2023-08-13 23:36:25 -0500

    Feat Adding a Card Component

--- my-app/src/app/pricing/page.jsx
+++ my-app/src/app/pricing/page.jsx
@@ -1,3 +1,4 @@
+import Card from "../components/Card"
 import { Stripe } from "stripe"
 
 async function loadPrices() {
@@ -20,17 +21,7 @@ async function PricingPage() {
         <header>
           <h1 className="my-5 text-center">Our Packs</h1>
         </header>
-        <div className="grid justify-center items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2">
-          {prices.map(price => (
-            <div key={price.id} className=" mb-2 border-2 p-6 ">
-              <h3>{price.product.name}</h3>
-              <h2 className="text-2xl font-bold">{price.unit_amount / 100} $</h2>
-              <img src={price.product.images[0]} alt={price.product.name} className="w-40 h-40 object-cover" />
-              <button className="mt-4 p-3 border border-white rounded-md">BUY</button>
-            </div>
-          ))
-          }
-        </div>
+        <Card prices={prices}/>
       </div>
     </div>
   )
