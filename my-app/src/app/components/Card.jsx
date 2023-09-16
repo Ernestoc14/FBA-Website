a76f1782de80850d7b05280d1d5abb2d5b93fcf3
Ernestoc14 <descreed34@hotmail.com>
2023-09-15 21:04:38 -0500

    Still Needing a Files Restructuring

--- my-app/src/app/components/Card.jsx
+++ my-app/src/app/components/Card.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 function Card({ prices }) {
   return (
     <div className="grid justify-center items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2">
-          {prices.map(price => (
-            <div key={price.id} className=" mb-2 border-2 p-6 ">
-              <h3>{price.product.name}</h3>
-              <h2 className="text-2xl font-bold">{price.unit_amount / 100} $</h2>
-              <img src={price.product.images[0]} alt={price.product.name} className="w-40 h-40 object-cover" />
-              <button className="mt-4 p-3 border border-white rounded-md">BUY</button>
-            </div>
-          ))
-          }
+      {prices.map(price => (
+        <div key={price.id} className=" mb-2 border-2 p-6">
+          <h3>{price.product.name}</h3>
+          <h2 className="text-center text-2xl font-bold">{price.unit_amount / 100} $</h2>
+          <div className="flex justify-center items-center">
+            <img src={price.product.images} alt={price.product.name} className="w-40 h-40 object-cover" />
+          </div>
+          <button className="mt-4 p-3 border border-white rounded-md">BUY</button>
         </div>
+      ))
+      }
+    </div>
   )
 }
 
--- my-app/src/app/pricing/page.jsx
+++ my-app/src/app/pricing/page.jsx
@@ -5,7 +5,7 @@ async function loadPrices() {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
   const { data: prices } = await stripe.prices.list({
     active: true,
-    limit: 3,
+    limit: 3, //Fix when we have more than 3 products
     expand: ['data.product'],
   })
   return prices
@@ -21,7 +21,9 @@ async function PricingPage() {
         <header>
           <h1 className="my-5 text-center">Our Packs</h1>
         </header>
-        <Card prices={prices} />
+        <div className="flex justify-center items-center">
+          <Card prices={prices} />
+        </div>
       </div>
     </div>
   )
