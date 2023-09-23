846faf354d13ca7838bbbeedfdafeb5e3b74cce7
Ernestoc14 <descreed34@hotmail.com>
2023-09-22 22:58:45 -0500

    Feat Adding a Video Overlay in Hero Section

--- my-app/src/app/components/Hero.jsx
+++ my-app/src/app/components/Hero.jsx
@@ -4,22 +4,38 @@ import Image from "next/image"
 function Hero() {
   return (
     <div className="hero">
-            <div className="hero__image-container">
-                <div className="hero__image">
-                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                </div>
-                <div className="hero__image-overlay" />
-            </div>
-        ---------------
-        <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title">
-                    Find, book, or rent a car -- quiclky and easily!
-                </h1>
-                <p className="hero__subtitle">
-                    Streamline your car rental experience with our efforless booking process.
-                </p>
-            </div>
+      <div className="hero__image-container h-60">
+        <div className="hero__image border border-black">
+          <Image
+            // src="/rywyn-logo.jpeg"
+            src="/Rywyn-Horizontal-Logo.jpeg"
+            alt="hero"
+            fill
+            className="object-contain"
+          />
         </div>
+      </div>
+
+      <div className="flex-1 w-full">
+        <div className="flex flex-col justify-center text-center w-full mt-32 z-10 absolute">
+          <h1 className="text-white text-5xl font-sans font-black">
+            The Best Resistance Bands
+            <br />
+            In the Market!
+          </h1>
+          <p className="text-white mt-10">
+            Streamline your car rental experience with our efforless booking process.
+          </p>
+        </div>
+      </div>
+
+
+      <div className="hero__video-overlay border border-green-700 min-w-full">
+        <video autoPlay muted loop fill className="w-full">
+          <source src="/bg-video-hero.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
   )
 }
 
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 
 function HomePage() {
   return (
-    <main>
+    <main className="overflow-hidden w-full">
       <Hero />
       <h1 className="font-bold text-3xl flex text-center justify-center mt-12">Next Section</h1>
     </main>
