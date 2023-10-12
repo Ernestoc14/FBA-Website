943903be902c7c3c764a1888855a2294801c8237
Ernestoc14 <descreed34@hotmail.com>
2023-10-11 21:59:24 -0500

    Feat Adding Styles to the CustomersReview Component

--- my-app/src/app/components/CustomersReview.jsx
+++ my-app/src/app/components/CustomersReview.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const reviews = [
-  { author: "Carlos Cedeno", comment: "It is a very good product, I use it for my injury recovey " },
-  { author: "Grandy Cassino", comment: "My girlfriend loves it, it was a birthday gift for her" },
-  { author: "Dario Crespo", comment: "I bought the Resistance Bands Pack 3 months ago for streaching and they are really good." },
-  { author: "Rita Caballero", comment: "I use the Bands in my workout everiday." }
-]
+  {
+    author: "Carlos Cedeno",
+    comment: "It is a very good product, I use it for my injury recovey.",
+  },
+  {
+    author: "Grandy Cassino",
+    comment: "My girlfriend loves it, it was a birthday gift for her.",
+  },
+  {
+    author: "Dario Crespo",
+    comment:
+      "I bought the Resistance Bands Pack 3 months ago for streaching and they are really good.",
+  },
+  {
+    author: "Rita Caballero",
+    comment: "I use the Bands in my workout everiday.",
+  },
+];
 
 function CustomersReview() {
   return (
-    <section className='flex flex-col w-full h-80 justify-center items-center text-white bg-black'>
-      <h2 className=' font-bold text-2xl'>Customer Reviews</h2>
-      <div className='grid grid-cols-2 gap-10 mt-8'>
+    <section className="flex flex-col w-full h-80 justify-center items-center text-white bg-black">
+      <h2 className=" font-bold text-2xl">Customer Reviews</h2>
+      <div className="grid grid-cols-2 gap-10 mt-8 place-items-center">
         {reviews.map((review, index) => (
-          <div key={index} className='border border-white'>
-            <h3 className='font-semibold text-lg'>{review.author}</h3>
-            <p className='w-2/3'>{review.comment}</p>
+          <div
+            key={index}
+            className="border border-white flex flex-col justify-center w-3/4"
+          >
+            <h3 className="font-semibold text-lg">{review.author}</h3>
+            <p className="">{review.comment}</p>
           </div>
         ))}
       </div>
     </section>
-  )
+  );
 }
 
-export default CustomersReview
+export default CustomersReview;
